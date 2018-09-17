ac2e5c679716e8030b0a01aa7c432b30017d41f3
shadhin056 <shadhinemail@gmail.com>
2018-09-17 11:12:05 +0600

    service added

--- src/main/java/com/example/restfulapiproject/Service/RestDAO.java
+++ src/main/java/com/example/restfulapiproject/Service/RestDAO.java
@@ -1,4 +1,21 @@
 package com.example.restfulapiproject.Service;
 
+import com.example.restfulapiproject.Model.User;
+import com.example.restfulapiproject.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public class RestDAO {
+    @Autowired
+    UserRepository userRepository;
+    //save in db
+    public User save(User user){
+        return userRepository.save(user);
+    }
+    //get users
+    public List<User>findAll(){
+        return userRepository.findAll();
+    }
 }

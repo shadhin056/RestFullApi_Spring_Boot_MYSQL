872b419387b03264d86b5bf51aafab7883a87b87
shadhin056 <shadhinemail@gmail.com>
2018-09-17 10:50:13 +0600

    init

---
+++ src/main/java/com/example/restfulapiproject/RestfulapiprojectApplication.java
@@ -0,0 +1,12 @@
+package com.example.restfulapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulapiprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulapiprojectApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/restfulapiproject/RestfulapiprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.restfulapiproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulapiprojectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

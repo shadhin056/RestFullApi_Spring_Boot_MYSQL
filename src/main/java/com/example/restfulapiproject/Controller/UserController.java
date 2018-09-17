64d55a1d6aec33ae019999745225552873840add
shadhin056 <shadhinemail@gmail.com>
2018-09-17 11:14:54 +0600

    controller added

--- src/main/java/com/example/restfulapiproject/Controller/UserController.java
+++ src/main/java/com/example/restfulapiproject/Controller/UserController.java
@@ -1,4 +1,27 @@
 package com.example.restfulapiproject.Controller;
 
+import com.example.restfulapiproject.Model.User;
+import com.example.restfulapiproject.Service.RestDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/rest")
 public class UserController {
+    @Autowired
+    RestDAO restDAO;
+
+    //save user
+    @PostMapping(path = "/user")
+    public User createUser(@Valid @RequestBody User user) {
+        return restDAO.save(user);
+    }
+    // get all users
+    @GetMapping(path = "/user")
+    public List<User> getAlluser() {
+        return restDAO.findAll();
+    }
 }
